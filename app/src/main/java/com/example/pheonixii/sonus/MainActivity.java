662429a08ce076d9e26baada0436913fe20508a2
Hysrok <doom_tew_u@yahoo.com>
2018-06-06 12:07:44 -0600

    WHO?!?!

--- app/src/main/java/com/example/pheonixii/sonus/MainActivity.java
+++ app/src/main/java/com/example/pheonixii/sonus/MainActivity.java
@@ -4,7 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-//I made a comment
+// I made a comment
+    // Who done it?!?
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

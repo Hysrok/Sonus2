7157df5dfb09ce5bbce4508906a76f4df6b76038
joerockclimber <joerockclimber@gmail.com>
2018-06-06 13:10:01 -0600

    Change

--- app/src/main/java/com/example/pheonixii/sonus/MainActivity.java
+++ app/src/main/java/com/example/pheonixii/sonus/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-//I made a comment // My butt
+//I made a comment // Another comment
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

453ea8cc7d3ce383420166ad0f82ed4fc8539d46
jbatson44 <bat14009@byui.edu>
2018-06-12 17:36:45 -0600

    Merge remote-tracking branch 'remotes/origin/master'
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/com/example/pheonixii/sonus/GameActivity.java
+++ app/src/main/java/com/example/pheonixii/sonus/GameActivity.java
@@ -1,7 +1,9 @@
 package com.example.pheonixii.sonus;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class GameActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        Intent intent = getIntent();
+    }
+
+    public void goHome(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/pheonixii/sonus/MainActivity.java
+++ app/src/main/java/com/example/pheonixii/sonus/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.pheonixii.sonus;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 //I made a comment // Another comment // Once again
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void play(View view) {
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
     }
 }

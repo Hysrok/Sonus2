99350c01d3c1344cfcb8f27eac97b3e486f6882d
jbatson44 <bat14009@byui.edu>
2018-06-19 16:58:56 -0600

    Merge branch 'master' of https://github.com/Hysrok/Sonus2
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser
    #	app/src/main/java/com/example/pheonixii/sonus/MainActivity.java

--- app/src/main/java/com/example/pheonixii/sonus/GameActivity.java
+++ app/src/main/java/com/example/pheonixii/sonus/GameActivity.java
@@ -22,11 +22,10 @@ public class GameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_game);
 
         Intent intent = getIntent();
-
+        ArrayList intervals = intent.getStringArrayListExtra("interval_list");
         Spinner spinner = (Spinner) findViewById(R.id.intervals_spinner);
 // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.intervals_array, android.R.layout.simple_spinner_item);
+        ArrayAdapter<String> adapter =  new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, intervals);
 // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 // Apply the adapter to the spinner

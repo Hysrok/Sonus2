b944cc0fe55d42019a7161a186e987e9b0ea9827
joerockclimber <joerockclimber@gmail.com>
2018-06-19 16:30:44 -0600

    Merge branch 'master' of https://github.com/Hysrok/Sonus2
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/java/com/example/pheonixii/sonus/MainActivity.java

--- app/src/main/java/com/example/pheonixii/sonus/SoundTask.java
+++ app/src/main/java/com/example/pheonixii/sonus/SoundTask.java
@@ -10,18 +10,16 @@ public class SoundTask extends AsyncTask<Note,Note,Note> {
 
     @Override
     protected Note doInBackground(Note ... note) {
-        try{
-            Thread.sleep(4000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+
+
+        Play.midi(note[0]);
         return note[0];
     }
     @Override
     protected void onProgressUpdate(Note ... note) {
 
-        Play play = new Play();
-        play.midi(note[0]);
+
+
 
     }
 

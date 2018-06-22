af75064256d8bd2de524b896bc867d83b052e53b
joerockclimber <joerockclimber@gmail.com>
2018-06-21 18:25:35 -0600

    Merge branch 'master' of https://github.com/Hysrok/Sonus2
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser

--- app/src/main/java/com/example/pheonixii/sonus/GameActivity.java
+++ app/src/main/java/com/example/pheonixii/sonus/GameActivity.java
@@ -10,6 +10,8 @@ import android.widget.Spinner;
 
 import java.net.URI;
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
 
 import jm.music.data.Note;
 
@@ -18,6 +20,19 @@ import static jm.constants.Durations.WHOLE_NOTE;
 import static jm.constants.Pitches.C4;
 
 public class GameActivity extends AppCompatActivity {
+
+    private static final Map<Integer, Integer> Notes = new TreeMap<Integer,Integer>(){{put(48, R.raw.fourty_eight); put(49, R.raw.fourty_nine);
+        put(50, R.raw.fifty);put(51, R.raw.fifty_one);put(52, R.raw.fifty_two);put(53, R.raw.fifty_three);put(54, R.raw.fifty_four);put(55, R.raw.fifty_five);
+        put(56, R.raw.fifty_six);put(57, R.raw.fifty_seven);put(58, R.raw.fifty_eight);put(59, R.raw.fifty_nine);put(60, R.raw.sixty);put(61, R.raw.sixty_one);
+        put(62, R.raw.sixty_two);put(63, R.raw.sixty_three);put(64, R.raw.sixty_four);put(65, R.raw.sixty_five);put(66, R.raw.sixty_six);put(67, R.raw.sixty_seven);
+        put(68, R.raw.sixty_eight);put(69, R.raw.sixty_nine);put(70, R.raw.seventy);put(71, R.raw.seventy_one);put(72, R.raw.seventy_two);put(73, R.raw.seventy_three);
+        put(74, R.raw.seventy_four);put(75, R.raw.seventy_five);put(76, R.raw.seventy_six);put(77, R.raw.seventy_seven);put(78, R.raw.seventy_eight);put(79, R.raw.seventy_nine);
+        put(80, R.raw.eighty);put(81, R.raw.eighty_one);put(82, R.raw.eighty_two);put(83, R.raw.eighty_three);put(84, R.raw.eighty_four);put(85, R.raw.eighty_five);
+        put(86, R.raw.eighty_six);put(87, R.raw.eighty_seven);put(88, R.raw.eighty_eight);put(89, R.raw.eighty_nine);put(91, R.raw.ninety_one);put(92, R.raw.ninety_two);
+        put(93, R.raw.ninety_three);put(94, R.raw.ninety_four);put(95, R.raw.ninety_five);put(96, R.raw.ninety_six);put(97, R.raw.ninety_seven);put(98, R.raw.ninety_eight);
+        put(99, R.raw.ninety_nine);put(100, R.raw.one_hundred);put(101, R.raw.one_hundred_one);put(102, R.raw.one_hundred_two);put(103, R.raw.one_hundred_three);}};
+
+
 
     private int baseNote = 0;
     private int testNote = 0;
@@ -57,7 +72,7 @@ public class GameActivity extends AppCompatActivity {
         int file = R.raw.fifty_eight;
         midiFileMediaPlayer1 = MediaPlayer.create(this, file);
         midiFileMediaPlayer1.start();
-        midiFileMediaPlayer2 = MediaPlayer.create(this, R.raw.fourty_nine);
+        midiFileMediaPlayer2 = MediaPlayer.create(this, R.raw.eighty_nine);
         midiFileMediaPlayer1.setNextMediaPlayer(midiFileMediaPlayer2);
         midiFileMediaPlayer1.start();
 

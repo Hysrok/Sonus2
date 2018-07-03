79605a355854c140a5d9509ff4a09fa047d369f4
jbatson44 <bat14009@byui.edu>
2018-07-03 15:42:37 -0600

    stats page shows users score

--- app/src/main/java/com/example/pheonixii/sonus/GameActivity.java
+++ app/src/main/java/com/example/pheonixii/sonus/GameActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
@@ -98,7 +99,7 @@ public class GameActivity extends AppCompatActivity {
 
     boolean correct = false;
     Spinner spinner;
-    float score = 0;
+    double score = 0.0;
 
     private ArrayList<String> intervals;
 
@@ -149,6 +150,7 @@ public class GameActivity extends AppCompatActivity {
             startRound();
         } else {
             Intent intent = new Intent(this, Stats.class);
+            intent.putExtra("USER_SCORE", score);
             startActivity(intent);
         }
     }
--- app/src/main/java/com/example/pheonixii/sonus/Stats.java
+++ app/src/main/java/com/example/pheonixii/sonus/Stats.java
@@ -7,13 +7,21 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 public class Stats extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_stats);
+
+        Intent intent = getIntent();
+        setContentView(R.layout.content_stats);
+       // int score = 0;
+        double score = intent.getDoubleExtra("USER_SCORE", 0.0);
+        TextView scoreText = (TextView)findViewById(R.id.textView);
+        scoreText.setText("Your Score: " + score + "/10");
+        /*setContentView(R.layout.content_stats);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -24,7 +32,7 @@ public class Stats extends AppCompatActivity {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
     }
 
     public void goHome(View view) {

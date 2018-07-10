36543e6677c608df098758a4df02f7589ca362c4
Hysrok <doom_tew_u@yahoo.com>
2018-07-10 17:18:25 -0600

    Moved the Notes and getters and setters to their own class.  Removed some unused variables, getters, and setters.

--- app/src/main/java/com/example/pheonixii/sonus/GameActivity.java
+++ app/src/main/java/com/example/pheonixii/sonus/GameActivity.java
@@ -14,72 +14,13 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.Map;
 import java.util.Random;
-import java.util.TreeMap;
 
 
 public class GameActivity extends AppCompatActivity {
     static final String SAVE_FILE = "";
 
-    private static final Map<Integer, Integer> Notes = new TreeMap<Integer, Integer>() {{
-        put(48, R.raw.fourty_eight);
-        put(49, R.raw.fourty_nine);
-        put(50, R.raw.fifty);
-        put(51, R.raw.fifty_one);
-        put(52, R.raw.fifty_two);
-        put(53, R.raw.fifty_three);
-        put(54, R.raw.fifty_four);
-        put(55, R.raw.fifty_five);
-        put(56, R.raw.fifty_six);
-        put(57, R.raw.fifty_seven);
-        put(58, R.raw.fifty_eight);
-        put(59, R.raw.fifty_nine);
-        put(60, R.raw.sixty);
-        put(61, R.raw.sixty_one);
-        put(62, R.raw.sixty_two);
-        put(63, R.raw.sixty_three);
-        put(64, R.raw.sixty_four);
-        put(65, R.raw.sixty_five);
-        put(66, R.raw.sixty_six);
-        put(67, R.raw.sixty_seven);
-        put(68, R.raw.sixty_eight);
-        put(69, R.raw.sixty_nine);
-        put(70, R.raw.seventy);
-        put(71, R.raw.seventy_one);
-        put(72, R.raw.seventy_two);
-        put(73, R.raw.seventy_three);
-        put(74, R.raw.seventy_four);
-        put(75, R.raw.seventy_five);
-        put(76, R.raw.seventy_six);
-        put(77, R.raw.seventy_seven);
-        put(78, R.raw.seventy_eight);
-        put(79, R.raw.seventy_nine);
-        put(80, R.raw.eighty);
-        put(81, R.raw.eighty_one);
-        put(82, R.raw.eighty_two);
-        put(83, R.raw.eighty_three);
-        put(84, R.raw.eighty_four);
-        put(85, R.raw.eighty_five);
-        put(86, R.raw.eighty_six);
-        put(87, R.raw.eighty_seven);
-        put(88, R.raw.eighty_eight);
-        put(89, R.raw.eighty_nine);
-        put(90, R.raw.ninety);
-        put(91, R.raw.ninety_one);
-        put(92, R.raw.ninety_two);
-        put(93, R.raw.ninety_three);
-        put(94, R.raw.ninety_four);
-        put(95, R.raw.ninety_five);
-        put(96, R.raw.ninety_six);
-        put(97, R.raw.ninety_seven);
-        put(98, R.raw.ninety_eight);
-        put(99, R.raw.ninety_nine);
-        put(100, R.raw.one_hundred);
-        put(101, R.raw.one_hundred_one);
-        put(102, R.raw.one_hundred_two);
-        put(103, R.raw.one_hundred_three);
-    }};
+    VerifyNotes verifyNotes = new VerifyNotes();
 
     private String interval;
     // ImageViews to remember which view was used last in order to delete them later.
@@ -89,11 +30,6 @@ public class GameActivity extends AppCompatActivity {
     private ImageView sharpU = null;
 
     int highestNote = 82;
-    private int baseNoteKey = 0;
-    private int baseNote = 0;
-    private int testNote = 0;
-    private int testNoteKey = 0;
-    private int userNoteKey = 0;
     private int roundNum = 0;
 
     boolean correct = false;
@@ -101,22 +37,6 @@ public class GameActivity extends AppCompatActivity {
     double score = 0.0;
 
     private ArrayList<String> intervals;
-
-    /**
-     * SETTERS
-     */
-
-    public void setBaseNote(int baseNote) {
-        this.baseNote = baseNote;
-    }
-
-    public void setTestNote(int testNote) {
-        this.testNote = testNote;
-    }
-
-    public void setUserNote(int userNote) {
-        this.userNoteKey = userNote;
-    }
 
     /**
      * ON CREATE
@@ -150,7 +70,7 @@ public class GameActivity extends AppCompatActivity {
         interval = randomInterval();
         randomBaseNote(); //has to go before the test note
         intervalTestNote();
-        displayNote(baseNoteKey);
+        displayNote(verifyNotes.getBaseNoteKey());
         soundOff();
     }
 
@@ -168,10 +88,9 @@ public class GameActivity extends AppCompatActivity {
      * @param view
      */
     public void submit(View view) {
-        userNoteKey = getUserNote();
         //String interval = randomInterval();
         verifyAnswer();
-        //Boolean trueorFalse = verifyInterval();
+        //Boolean trueOrFalse = verifyInterval();
         Toast.makeText(this, "Score  " + score, Toast.LENGTH_LONG).show();
         //verifyAnswer();
         roundNum++;
@@ -468,14 +387,14 @@ public class GameActivity extends AppCompatActivity {
     public void soundOff() {
         MediaPlayer midiFileMediaPlayer1;
         MediaPlayer midiFileMediaPlayer2;
-        midiFileMediaPlayer1 = MediaPlayer.create(this, Notes.get(baseNoteKey));
+        midiFileMediaPlayer1 = MediaPlayer.create(this, VerifyNotes.Notes.get(verifyNotes.getBaseNoteKey()));
         midiFileMediaPlayer1.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
             @Override
             public void onCompletion(MediaPlayer mediaPlayer) {
                 mediaPlayer.release();
             }
         });
-        midiFileMediaPlayer2 = MediaPlayer.create(this, Notes.get(testNoteKey));
+        midiFileMediaPlayer2 = MediaPlayer.create(this, VerifyNotes.Notes.get(verifyNotes.getTestNoteKey()));
         midiFileMediaPlayer2.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
             @Override
             public void onCompletion(MediaPlayer mediaPlayer) {
@@ -567,8 +486,7 @@ public class GameActivity extends AppCompatActivity {
     public void randomBaseNote() {
         int maxNote = highestNote - convertIntervalToInt();
         Random rand = new Random();
-        baseNoteKey = rand.nextInt((maxNote - 60) + 1) + 60; //rand.nextInt((max - min) + 1) + min;
-        setBaseNote(Notes.get(baseNoteKey));
+        verifyNotes.setBaseNoteKey(rand.nextInt((maxNote - 60) + 1) + 60); //rand.nextInt((max - min) + 1) + min;
     }
 
 
@@ -617,10 +535,8 @@ public class GameActivity extends AppCompatActivity {
      * Get test note base off the base note and interval
      **********************/
     public void intervalTestNote() {
-        testNoteKey = baseNoteKey + convertIntervalToInt();
+        verifyNotes.setTestNoteKey(verifyNotes.getBaseNoteKey() + convertIntervalToInt());
        // Toast.makeText(this, "Note = " + baseNoteKey + " Note2 = " + testNoteKey, Toast.LENGTH_LONG).show();
-        if(Notes.get(testNoteKey) != null)
-            setTestNote(Notes.get(testNoteKey));
     }
 
     /**
@@ -641,7 +557,7 @@ public class GameActivity extends AppCompatActivity {
      * @return
      */
     public boolean verifyNote() {
-        if(baseNoteKey == getUserNote())
+        if(verifyNotes.getBaseNoteKey() == getUserNote())
         {
             Toast.makeText(this, "True", Toast.LENGTH_SHORT).show();
         return true;}
